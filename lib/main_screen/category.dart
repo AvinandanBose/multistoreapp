85788296902340d913a48ef51959036087446721
AvinandanBose <boseavinandan@gmail.com>
2022-10-04 10:37:30 +0530

    First Update

--- lib/main_screen/category.dart
+++ lib/main_screen/category.dart
@@ -9,9 +9,32 @@ class CategoryScreen extends StatefulWidget {
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
+
+  Widget sideNavigator(Size size){
+    return Container(
+      height: size.height*0.8,
+      //It means the container's height is 80% of total height of the screen
+      width: size.width*0.2,
+      //It means the container's width is 20% of total width of the screen
+      color: Colors.grey.shade300,
+    );
+
+  }
+
+  Widget categView(Size size){
+    return Container(
+      height: size.height*0.8,
+      //It means the container's height is 80% of total height of the screen
+      width: size.width*0.8,
+      //It means the container's width is 80% of total width of the screen
+      color: Colors.white,
+    );
+  }
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    var sizeNew = MediaQuery.of(context).size;
+
+    return Scaffold(
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.white,
@@ -19,16 +42,19 @@ class _CategoryScreenState extends State<CategoryScreen> {
       ),
       body: Stack(
         children: <Widget>[
-          Positioned(child: Container(
-            height: 500,
-            width: 100,
-            color: Colors.red,
-            //by default it will align to top left corner
-            //but we need here is bottom left corner.
+          Positioned(
+            //Aligning to bottom left corner
+            bottom: 0,
+            left: 0,
+            child: sideNavigator(sizeNew),
           ),
+          Positioned(
+            //Aligning to bottom left corner
+            bottom: 0,
+            right: 0,
+            child: categView(sizeNew),
           ),
         ],
-
       ),
     );
   }
