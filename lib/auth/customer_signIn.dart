bd5f83f0a30961a4cb8e83d645e01e09decb43a5
AvinandanBose <boseavinandan@gmail.com>
2022-11-16 02:07:28 +0530

    SnackbarDesign5

--- lib/auth/customer_signIn.dart
+++ lib/auth/customer_signIn.dart
@@ -234,11 +234,20 @@ class MyMessageHandler{
      SnackBar(
         duration: const Duration(seconds: 2),
         backgroundColor: Colors.yellow,
+        padding: const EdgeInsets.symmetric( vertical: 10),
+       shape: const RoundedRectangleBorder(
+         side: BorderSide(
+           color: Colors.black,
+         ),
+         borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight:Radius.circular(20) ),
+       ),
         content: Text(
           message!,
           textAlign: TextAlign.center,
           style: const TextStyle(
             fontSize: 18,
+            fontFamily: 'Acme',
+            fontWeight: FontWeight.bold,
             color: Colors.black,
           ),
         ),
