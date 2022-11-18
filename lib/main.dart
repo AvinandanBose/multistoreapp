ecc8c50d4551f1e035c71b4cfa6b5779ccf79e84
AvinandanBose <boseavinandan@gmail.com>
2022-11-18 18:43:47 +0530

    FirebaseFlutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:multistoreapp/auth/customer_signIn.dart';
 import 'package:multistoreapp/main_screen/customer_home.dart';
 import 'package:multistoreapp/main_screen/supplierhomescreen.dart';
 import 'package:multistoreapp/main_screen/welcome_screen.dart';
-void main(){
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -12,16 +16,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       //home: WelcomeScreen(),
 
       initialRoute: 'welcome_screen',
       routes: {
-        'welcome_screen' : (context) => const WelcomeScreen(),
-        'customer_home' : (context) => const CustomerHomeScreen(),
-        'supplier_home' : (context) => const SupplierHomeScreen(),
-        'customer_signup' : (context) => const CustomerRegister(),
+        'welcome_screen': (context) => const WelcomeScreen(),
+        'customer_home': (context) => const CustomerHomeScreen(),
+        'supplier_home': (context) => const SupplierHomeScreen(),
+        'customer_signup': (context) => const CustomerRegister(),
       },
     );
   }
